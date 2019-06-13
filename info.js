5fc70eb43404f0ed265ec058ea2898731b05f569
Alexander Darland <alexander.darland@creuna.se>
2019-06-13 11:54:28 +0200

    Updated default values for info.js

--- info.js
+++ info.js
@@ -1,6 +1,6 @@
 /* TODO: Add your own team name and the people in your group under authors */
 
 var info = {
-  teamName: 'The Hoffs',
-  authors: ['Alexander', 'Fredrik']
+  teamName: 'Team Name',
+  authors: ['Author 1', 'Author 2']
 }
